7335c821365042b43c05797fcd13fc6dc1dcd54f
sup4mile <55961426+sup4mile@users.noreply.github.com>
2023-08-21 10:52:07 -0500

    Revise plots for benchmark calibration

--- julia/codes_w_exo_wage/plots_benchmark.jl
+++ julia/codes_w_exo_wage/plots_benchmark.jl
@@ -1,5 +1,15 @@
 # Generate plots for presentation slides:
 
+# Select type of parameterization (from following list):
+    # (1) benchmark
+    # (2) counter_1
+    # (3) counter_2
+    # (4) high_beta
+    # (5) low_beta
+    # (6) high_beta_high_sigma
+    # (7) low_beta_low_sigma
+    paramname = "benchmark"
+    pathname = string("./plots/",paramname,"/")
 # Select backend:
 pyplot()
 # (1) 'year' = 1970
@@ -17,11 +27,13 @@ plt3 = scatter(τ_w[:,1],label=string(year),markershape=:circle)
 # (1.b.1) Create plot for 1970 only:
 plt3_1_70 = scatter(τ_w[:,1],label=string(year),markershape=:circle,ylims=(-1.5,1),legend=:left)
 plot!(plt3_1_70,ylabel=L"τ_w",title="Labor Market Barriers Against Women",grid=false,subplot=1)
-savefig(plt3_1_70,"./plots/tau_w_women_70.eps")
+filename = string(pathname,"tau_w_women_70.eps")
+savefig(plt3_1_70,filename)
 # (1.b.2) Scatter plot of occupational productivities:
 plt3_2_70 = scatter(a_by_occ,label=string(year),markershape=:circle,legend=:topleft,ylims=(0,1.25))
 plot!(plt3_2_70,ylabel=L"A_o",title="Occupational Productivities",grid=false,subplot=1)
-savefig(plt3_2_70,"./plots/A_men_70.eps")
+filename = string(pathname,"A_men_70.eps")
+savefig(plt3_2_70,filename)
 # (1.c) Distribution of 'e_T'
 plt4 = plot(a_grid[2:end-2],e_T[2:end-2,iHH,1],label=string(year))
 # plt5 = plot(a_grid[2:end-2],e_T[2:end-2,iHH,2],label=string(year))
@@ -48,10 +60,12 @@ plot!(plt2_2,h_T[2:end-2,iHH,2],f_T[2:end-2,iHH,2],label=string(year))
 scatter!(plt3,τ_w[:,1],label=string(year),markershape=:diamond)
 # (2.b.1) Create plot for 1970-90 only:
 plt3_1_70_90 = scatter!(plt3_1_70,τ_w[:,1],label=string(year),markershape=:diamond,ylims=(-1.5,1))
-savefig(plt3_1_70_90,"./plots/tau_w_women_70_90.eps")
+filename = string(pathname,"tau_w_women_70_90.eps")
+savefig(plt3_1_70_90,filename)
 # (2.b.2) Create plot for 1970-90 only:
 plt3_2_70_90 = scatter!(plt3_2_70,a_by_occ,label=string(year),markershape=:diamond,ylims=(0,1.25))
-savefig(plt3_2_70_90,"./plots/A_men_70_90.eps")
+filename = string(pathname,"A_men_70_90.eps")
+savefig(plt3_2_70_90,filename)
 # (2.c)
 plot!(plt4,a_grid[2:end-2],e_T[2:end-2,iHH,1],label=string(year))
 # plot!(plt5,a_grid[2:end-2],e_T[2:end-2,iHH,2],label=string(year))
@@ -74,7 +88,8 @@ plot!(plt2_2,h_T[2:end-2,iHH,2],f_T[2:end-2,iHH,2],label=string(year))
 scatter!(plt3,τ_w[:,1],label=string(year),markershape=:xcross)
 # (2.b.1) Create plot for 1970-2010 only:
 plt3_1_70_10 = scatter!(plt3_1_70_90,τ_w[:,1],label=string(year),markershape=:xcross)
-savefig(plt3_1_70_10,"./plots/tau_w_women_70_10.eps")
+filename = string(pathname,"tau_w_women_70_10.eps")
+savefig(plt3_1_70_10,filename)
 # (2.b.2) Create plot for 1970-2010 only:
 plt3_2_70_10 = scatter!(plt3_2_70_90,a_by_occ,label=string(year),markershape=:xcross,ylims=(0,1.25))
 savefig(plt3_2_70_10,"./plots/A_men_70_10.eps")
